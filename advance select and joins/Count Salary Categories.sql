524ef11968c5e5e947890c676f522f0a7c6521da
LegasriMasanam <142808637+Legasri-Masanam@users.noreply.github.com>
2025-08-06 11:47:56 +0530

    Add files via upload

---
+++ advance select and joins/Count Salary Categories.sql
@@ -0,0 +1,11 @@
+select 'Low Salary' as category ,
+    count(if( income < 20000, 1, null)) as accounts_count
+FROM Accounts
+
+union all
+select 'Average Salary',
+    count(if(income >= 20000 and income <= 50000 , 1, null)) from Accounts
+union all
+
+select 'High Salary',
+    count(if( income > 50000 , 1, null)) from accounts
